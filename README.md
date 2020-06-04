b3d8541e00a5f76820e37724ebb479164d7b654e
Manfred Kaiser <manfred.kaiser@logfile.at>
2020-06-04 15:24:55 +0200

    initial pip package

---
+++ README.md
@@ -0,0 +1,9 @@
+# python-enhancements - Enhanced Python Classes
+
+[![Github version](https://img.shields.io/github/v/release/manfred-kaiser/python-enhancements?label=github&logo=github)](https://github.com/manfred-kaiser/python-enhancements/releases)
+[![PyPI version](https://img.shields.io/pypi/v/enhancements.svg?logo=pypi&logoColor=FFE873)](https://pypi.org/project/enhancements/)
+[![Supported Python versions](https://img.shields.io/pypi/pyversions/enhancements.svg?logo=python&logoColor=FFE873)](https://pypi.org/project/enhancements/)
+[![PyPI downloads](https://img.shields.io/pypi/dm/python-enhancements.svg)](https://pypistats.org/packages/enhancements)
+[![GitHub](https://img.shields.io/github/license/manfred-kaiser/python-enhancements.svg)](LICENSE)
+
+`enhancements` is a python library which extends various python classes.
---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='enhancements',
+    version='0.0.1',
+    author='Manfred Kaiser',
+    author_email='manfred.kaiser@logfile.at',
+    description='utility library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>= 3.5',
+    packages=find_packages(),
+    url="https://github.com/manfred-kaiser/python-enhancements",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)"
+    ],
+)
