71f6f781ee6b105439ed4ef642452d933263561e
Manfred Kaiser <manfred.kaiser@logfile.at>
2020-09-22 11:13:20 +0200

    added py.typed file for typing support

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages  # type: ignore
+from setuptools import setup, find_packages
 
 # read the contents of your README file
 from os import path
@@ -18,7 +18,10 @@ setup(
     long_description_content_type='text/markdown',
     keywords="development configparser argparse",
     python_requires='>= 3.6',
-    packages=find_packages(exclude=("tests",)),  # type: ignore
+    packages=find_packages(exclude=("tests",)),
+    package_data={
+        'enhancements': ['py.typed'],
+    },
     url="https://enhancements.readthedocs.io/",
     project_urls={
         'Source': 'https://github.com/manfred-kaiser/python-enhancements',
